bd089d9a96b61c94cfd624bcafd61bdf1ef4d575
Rudrava <rudravamukherjee.2001@gmail.com>
2020-10-12 11:56:18 +0530

    learnt bout props and its childern

--- src/index.js
+++ src/index.js
@@ -4,29 +4,54 @@ import ReactDom from "react-dom";
 // :: CSS ::
 import "./index.css";
 
+//  :: SET UP VARS ::
+const firstBook = {
+	title : "I don't know",
+	name : "Rudrava Mukherjee",
+	imgLink : "https://via.placeholder.com/150"
+}
+
+const secondBook = {
+	title : "I still don't know",
+	name : "Rudra",
+	imgLink : "https://via.placeholder.com/150"
+}
+
 //BOOK list COMPONENT
 function BookList() {
 	return (
 		<section className="bookList">
-			<Book />
+			<Book 
+				title = { firstBook.title } 
+				name = { firstBook.name }
+				imgLink= { firstBook.imgLink }
+			>
+			<p>
+		{/*this is a child*/}
+				非法汉化版搜索奇迹一
+				次国语经销商不需要帮
+				我报价试验邮箱，打。
+			</p>	
+
+			</Book>
+			<Book 
+				title = { secondBook.title } 
+				name = { secondBook.name }
+				imgLink= { secondBook.imgLink }
+			/>
 		</section>
 	);
 }
 
-// Book component
-const Book = () => {
-	// :: normal js here ::
-	const title = "I dont know"
-	const name = "Rudrava Mukherjee"
+// adding param to the components
+// accessing a child like so
+const Book = ({ title, name, imgLink, children}) => {
 	return (
 		<article className="book">
-			<img src="https://via.placeholder.com/150" alt="img" />
-			{/* :: ADDING  JS VARIABLE INSIDE JSX :: */}
+			<img src={imgLink} alt="img" />
 			<h1>{title}</h1>
 			<h4> {name}</h4>
-			
-			{/*:: err ::*/}
-			{/* <h4> {name = "someone"}</h4> */}
+			<p> {children} </p>
 		</article>
 	);
 }
